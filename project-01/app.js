db1fc69ca13ff69c1a4e065fbc753c97b9f359d2
glaucia86 <glaucia_lemos86@hotmail.com>
2023-01-27 17:53:14 -0300

    initial commit

---
+++ project-01/app.js
@@ -0,0 +1,18 @@
+/**
+ * file: app.js
+ * date: 01/27/2023
+ * description: file responsible for the starting the application
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá, pessoal!');
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
